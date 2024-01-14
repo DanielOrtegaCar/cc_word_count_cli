f67f822e39cad424ce905a81165fabce8902e1a6
DanielOrtega94 <danielortegacarcamo@gmail.com>
2024-01-14 13:24:50 -0300

    update the code for step 2, add unit test, coverage, linters, and nox

--- wc_cli/cli.py
+++ wc_cli/cli.py
@@ -1,13 +1,13 @@
 # from funciones import count
 import click
 import sys
-from typing import Tuple,IO,List,Union
+from typing import Tuple, IO, List, Union,TypeAlias
 
+result_type: TypeAlias = Tuple[str, str, str, str, str]
 
-def count(text_data:IO[bytes])->Tuple[str,str,str,str,str]:
+def count(text_data: IO[bytes]) -> result_type:
     """
     count : Count bytes, words, characters of text data
-
 
     Args:
         text_data (IO[bytes]): text to count elements
@@ -20,21 +20,43 @@ def count(text_data:IO[bytes])->Tuple[str,str,str,str,str]:
     max_line_len = -9999
 
     for line in text_data:
-        if max_line_len< len(line): 
+        if max_line_len < len(line):
             max_line_len = len(line)
         byte_count += len(line)
         word_count += len(line.split())
-        char_count += len(line.decode() )
-        
-        if line.decode().endswith('\n'):
+        char_count += len(line.decode())
+
+        if line.decode().endswith("\n"):
             newlines += 1
 
-    return str(newlines), str(word_count), str(char_count), str(byte_count),str(max_line_len)
-    
+    return (
+        str(newlines),
+        str(word_count),
+        str(char_count),
+        str(byte_count),
+        str(max_line_len),
+    )
 
-def get_result(filename:str,result:tuple,count_bytes:bool,count_lines:bool)->str:
-    newlines, word_count, char_count, byte_count,max_line_len = result
-    
+
+def get_result(
+    filename: str, result: result_type, count_bytes: bool, count_lines: bool
+) -> str:
+    """
+    get_result _summary_
+
+    function to generate the result string, given the parameters
+
+    Args:
+        filename (str): _description_
+        result (tuple): _description_
+        count_bytes (bool): _description_
+        count_lines (bool): _description_
+
+    Returns:
+        str: _description_
+    """
+    newlines, word_count, char_count, byte_count, max_line_len = result
+
     text = ""
 
     if count_lines:
@@ -42,7 +64,6 @@ def get_result(filename:str,result:tuple,count_bytes:bool,count_lines:bool)->str
 
     if count_bytes:
         text = text + " {} ".format(byte_count)
-
 
     # como manejar el caso por default (?), que deberia mostrar todos
     else:
@@ -52,12 +73,10 @@ def get_result(filename:str,result:tuple,count_bytes:bool,count_lines:bool)->str
     return text
 
 
+def word_count(files: Union[str, List[str]], count_bytes: bool, count_lines: bool):
+    # newline,  word, and byte
 
-def word_count(files:Union[str,List[str]], count_bytes:bool,count_lines:bool):
-
-    #newline,  word, and byte 
-    
-    #newline, word,  character,  byte,  maximum line length
+    # newline, word,  character,  byte,  maximum line length
     if not files:
         filename = ""
         files = ["-"]
@@ -65,18 +84,16 @@ def word_count(files:Union[str,List[str]], count_bytes:bool,count_lines:bool):
     for file in files:
         filename = file
 
-
         if file == "-":
             result = count(text_data=sys.stdin.buffer)
         else:
             with open(file, "rb") as file_data:
                 result = count(text_data=file_data)
 
-
-        result = get_result(filename,result,count_bytes,count_lines)
+        nuevo_resultado = get_result(filename, result, count_bytes, count_lines)
         # print(result)
- 
-        click.echo(result)
+
+        click.echo(nuevo_resultado)
 
 
 @click.command()
@@ -86,19 +103,18 @@ def word_count(files:Union[str,List[str]], count_bytes:bool,count_lines:bool):
     "--bytes",
     "count_bytes",
     is_flag=True,
-    help="The number of bytes in each input file is written to the standard output."
+    help="The number of bytes in each input file is written to the standard output.",
 )
 @click.option(
     "-l",
     "--lines",
     "count_lines",
     is_flag=True,
-    help="The number of lines in the file."
+    help="The number of lines in the file.",
 )
-def cli(files,count_bytes,count_lines):
-
-    print(files,count_bytes,count_lines)
-    word_count(files,count_bytes,count_lines)
+def cli(files, count_bytes, count_lines):
+    print(files, count_bytes, count_lines)
+    word_count(files, count_bytes, count_lines)
 
 
 if __name__ == "__main__":
