ca0c824a3f572520fc974116304eb3255246e8f6
DanielOrtega94 <danielortegacarcamo@gmail.com>
2024-01-14 15:29:32 -0300

    add code for final steps and make some unit test

--- tests/test.py
+++ tests/test.py
@@ -16,14 +16,17 @@ class TestCli(unittest.TestCase):
         with open(filename, "rb") as file_data:
             result = cli.count(text_data=file_data)
 
-        test_1 = cli.get_result(filename, result, False, False)
+        test_1 = cli.get_result(filename, result, False, False,False,False)
         self.assertEqual(test_1, "4 67 375 385 171 tests/test.txt")
 
-        test_2 = cli.get_result(filename, result, True, False)
+        # test_4 = cli.get_result("", result, False, False,False,False)
+        # self.assertEqual(test_1, "4 67 375 385 171 tests/test.txt")
+
+        test_2 = cli.get_result(filename, result, True, False,False,False)
         self.assertEqual(test_2, " 385  tests/test.txt")
 
-        test_3 = cli.get_result(filename, result, True, True)
-        self.assertEqual(test_3, " 4  385  tests/test.txt")
+        test_3 = cli.get_result(filename, result, True, True,True, True)
+        self.assertEqual(test_3, ' 4  67  385  375  tests/test.txt')
 
 
 if __name__ == "__main__":
